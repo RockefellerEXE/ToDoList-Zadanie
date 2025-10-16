5d1a797c208f0ce550c20c3bb9c61be5176cdcff
Adam Wujkiewicz <adamwwujkiewicz@gmail.com>
2025-10-16 19:59:02 +0200

    Add basic create,delete,edit

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -14,11 +14,54 @@ namespace ToDoList.Controllers
         }
         public async Task<IActionResult> Index()
         {
-            var item = _context.Items.ToListAsync();
+            var item = await _context.Items.ToListAsync();
             return View(item);
         }
         public IActionResult Create() { 
             return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create([Bind("Id, Name, Description, Finished")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Items.Add(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Edit(int id)
+        {
+            var item = _context.Items.FirstOrDefault(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, [Bind("Id, Name, Description, Finished")] Item item)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(item);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            return View(item);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var item = _context.Items.FirstOrDefault(x => x.Id == id);
+            return View(item);
+        }
+        [HttpPost, ActionName("Delete")]
+        public async Task<IActionResult> DeleteItem(int id)
+        {
+            var item = await _context.Items.FindAsync(id);
+            if (item != null)
+            {
+                _context.Items.Remove(item);
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
