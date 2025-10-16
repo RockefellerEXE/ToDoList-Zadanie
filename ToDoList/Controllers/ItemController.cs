1a173df90e2b9cc7d962bdca18ad75bfb06d2415
Adam Wujkiewicz <adamwwujkiewicz@gmail.com>
2025-10-16 18:53:39 +0200

    Add create template

--- ToDoList/Controllers/ItemController.cs
+++ ToDoList/Controllers/ItemController.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ToDoList.DAL;
 using ToDoList.Models;
 
 namespace ToDoList.Controllers
 {
     public class ItemController : Controller
     {
-        public IActionResult Index()
+        private readonly ItemContext _context;
+        public ItemController(ItemContext context)
         {
-            List<Item> lista = new List<Item>();
-            var item = new Item() { Name = "Testowa pozycja", Description = "Opis testowej pozycji", Finished = false };
-            var item2 = new Item() { Name = "Testowa pozycja2", Description = "Opis testowej pozycji2", Finished = true };
-            lista.Add(item);
-            lista.Add(item2);
-            return View(lista);
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var item = _context.Items.ToListAsync();
+            return View(item);
+        }
+        public IActionResult Create() { 
+            return View();
         }
     }
 }
--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
-        public Boolean Finished { get; set; }
+        public bool Finished { get; set; }
 
 
     }
